64896ff915e8a004663d753d9f2ec2eb51e0d6be
Jeddy Tan <jeddy.tan@autodesk.com>
2025-08-05 04:26:08 +0000

    fix(food-maps): fix project graph errors
    - Add @next/eslint-plugin-next dependency
    - Update Jest configuration to use SWC
    - Fix ESLint configuration for Next.js

--- apps/food-maps/eslint.config.mjs
+++ apps/food-maps/eslint.config.mjs
@@ -1,21 +1,25 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import js from "@eslint/js";
-import { fixupConfigRules } from "@eslint/compat";
-import nx from "@nx/eslint-plugin";
-import baseConfig from "../../eslint.config.mjs";
+import nextPlugin from '@next/eslint-plugin-next';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
 });
 
 export default [
-  ...fixupConfigRules(compat.extends("next")),
-  ...fixupConfigRules(compat.extends("next/core-web-vitals")),
-  ...baseConfig,
-  ...nx.configs["flat/react-typescript"],
+  ...compat.extends('next/core-web-vitals'),
   {
-    ignores: [".next/**/*"],
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+    },
   },
 ];
--- apps/food-maps/jest.config.ts
+++ apps/food-maps/jest.config.ts
@@ -1,10 +1,13 @@
+/* eslint-disable */
 export default {
-  displayName: "food-maps",
-  preset: "../../jest.preset.js",
+  displayName: 'food-maps',
+  preset: '../../jest.preset.js',
   transform: {
-    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
-    "^.+\\.[tj]sx?$": "babel-jest",
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['@swc/jest'],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/apps/food-maps",
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/food-maps',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
 };
